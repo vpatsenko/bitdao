dccb25f3ec8e872cb91fc1d0cad9fb961dd85a3e
Vladimir Patsenko <v@patsenko.ru>
2022-04-30 12:44:50 +0300

    PullPayment

--- contracts/DAO.sol
+++ contracts/DAO.sol
@@ -1,175 +1,192 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/security/PullPayment.sol";
 
-contract DAO is Ownable {
-    event Received(address, uint256);
+contract DAO is Ownable, ReentrancyGuard, PullPayment {
+    // event Received(address, uint256);
 
-    struct Participant {
-        bool isVoted;
-        bool isParticipant;
-        uint256 voteAmount;
-    }
+    // struct Participant {
+    //     bool isVoted;
+    //     bool isParticipant;
+    //     uint256 voteAmount;
+    // }
 
-    struct Winner {
-        address winnerAddress;
-        uint256 voteAmount;
-    }
+    // struct Winner {
+    //     address winnerAddress;
+    //     uint256 voteAmount;
+    // }
 
-    mapping(address => Participant) public addressToParticipant;
+    // struct Election {
+    //     bool isElectionInited;
+    //     bool isPrizeWithdrawn;
+    //     bool isEnded;
+    //     uint256 deadline;
+    //     uint256 treasury;
+    //     Winner winner;
+    //     mapping(address => Participant) addressToParticipant;
+    // }
 
-    modifier isParticipant(address v) {
-        require(
-            true == addressToParticipant[v].isParticipant,
-            "address is not a participant"
-        );
-        require(
-            true == addressToParticipant[msg.sender].isParticipant,
-            "address is not a participant"
-        );
-        _;
-    }
+    // modifier isDeadlineReached(uint256 _electionID) {
+    //     if (electionsMapping[_electionID].deadline < block.timestamp) {
+    //         electionsMapping[_electionID].isElectionInited = false;
+    //         electionsMapping[_electionID].deadline = 0;
+    //         revert("election is finished");
+    //     }
+    //     _;
+    // }
 
-    modifier isDeadlineReached() {
-        if (deadline < block.timestamp) {
-            isElectionInited = false;
-            deadline = 0;
-            require(deadline > block.timestamp, "election is finished");
-        }
-        _;
-    }
+    // modifier isElectionInited(uint256 _electionID) {
+    //     require(
+    //         electionsMapping[_electionID].isElectionInited == true,
+    //         "election is not inited"
+    //     );
+    //     _;
+    // }
 
-    modifier isElectionInitedMod() {
-        require(isElectionInited == true, "election is not inited");
-        _;
-    }
+    // modifier isPrizeWithdrawn(uint256 _electionID) {
+    //     require(
+    //         electionsMapping[_electionID].isEnded == true,
+    //         "election is not ended"
+    //     );
+    //     require(
+    //         electionsMapping[_electionID].isElectionInited == true,
+    //         "prize is already withdrawn"
+    //     );
+    //     require(
+    //         electionsMapping[_electionID].winner.winnerAddress == msg.sender,
+    //         "address is not the winner"
+    //     );
+    //     _;
+    // }
 
-    modifier prizeWithdrawnMod() {
-        require(isPrizeWithdrawn == false, "prize is already withdrawn");
-        require(deadline == 0, "deadline should be equal to zero");
-        require(
-            isElectionInited == false,
-            "elections should be ended in order for the prize to be withdrawn"
-        );
-        require(
-            msg.sender == winner.winnerAddress,
-            "address is not the winner"
-        );
-        _;
-    }
+    // modifier isParticipatingInElections() {
+    //     require(
+    //         addressesToElectionIDs[msg.sender] == 0,
+    //         "address is not participating in the elections"
+    //     );
+    //     _;
+    // }
 
-    bool public isElectionInited;
-    bool public isPrizeWithdrawn;
+    // modifier isVoteAllowed(uint256 _electionID) {
+    //     require(
+    //         electionsMapping[_electionID]
+    //             .addressToParticipant[msg.sender]
+    //             .isVoted == false,
+    //         "address has voted already"
+    //     );
+    //     require(10000000000000000 == msg.value, "amount is not 0.01 eth");
+    //     _;
+    // }
 
-    uint256 public deadline;
-    uint256 public treasury;
-    uint256 public feePercentage;
+    // uint256 public feePercentage;
+    // uint256 public numberOfCurrentElections;
+    // uint256 public feeTreasury;
 
-    Winner private winner;
+    // mapping(uint256 => Election) public electionsMapping;
+    // mapping(address => uint256) public addressesToElectionIDs;
 
-    constructor() {
-        isElectionInited = false;
-        feePercentage = 20;
-    }
+    // constructor() {
+    //     feePercentage = 10;
+    //     numberOfCurrentElections = 1;
+    // }
 
-    struct Election {
-        bool isElectionInited;
-        bool isPrizeWithdrawn;
-        uint256 deadline;
-        uint256 treasury;
-        uint256 feePercentage;
-        Winner winner;
-        mapping(address => Participant) addressToParticipant;
-    }
+    // // returns election id which should be used in the other
+    // // function calls to identify the election user is participating in
+    // function addVoting(address[] memory participanAddressesList)
+    //     public
+    //     onlyOwner
+    //     returns (uint256)
+    // {
+    //     electionsMapping[numberOfCurrentElections].isElectionInited = true;
+    //     electionsMapping[numberOfCurrentElections].isPrizeWithdrawn = false;
 
-    mapping(uint256 => Election) public electionsMapping;
-    uint256 public numberOfCurrentElections;
+    //     electionsMapping[numberOfCurrentElections].deadline =
+    //         block.timestamp +
+    //         256200;
 
-    // returns election id which should be used in the other
-    // function calls to identify the election user is participating in
-    function initElections(address[] memory participanAddressesList)
-        public
-        onlyOwner
-        returns (uint256)
-    {
-        electionsMapping[numberOfCurrentElections].isElectionInited = true;
-        electionsMapping[numberOfCurrentElections].isPrizeWithdrawn = false;
+    //     for (uint256 i = 0; i < participanAddressesList.length; i++) {
+    //         Participant memory participant;
+    //         participant.isParticipant = true;
 
-        electionsMapping[numberOfCurrentElections].deadline =
-            block.timestamp +
-            256200;
+    //         electionsMapping[numberOfCurrentElections].addressToParticipant[
+    //                 participanAddressesList[i]
+    //             ] = participant;
+    //     }
 
-        for (uint256 i = 0; i < participanAddressesList.length; i++) {
-            Participant memory participant;
-            participant.isParticipant = true;
+    //     numberOfCurrentElections++;
+    //     return numberOfCurrentElections;
+    // }
 
-            electionsMapping[numberOfCurrentElections].addressToParticipant[
-                    participanAddressesList[i]
-                ] = participant;
-        }
+    // function vote(address _participant, uint256 _electionID)
+    //     public
+    //     payable
+    //     isParticipatingInElections
+    //     isVoteAllowed(_electionID)
+    //     isElectionInited(_electionID)
+    //     isDeadlineReached(_electionID)
+    // {
+    //     electionsMapping[_electionID]
+    //         .addressToParticipant[_participant]
+    //         .voteAmount += 1;
+    //     electionsMapping[_electionID]
+    //         .addressToParticipant[msg.sender]
+    //         .isVoted = true;
+    //     electionsMapping[_electionID].treasury += msg.value;
 
-        numberOfCurrentElections++;
-        return numberOfCurrentElections;
-    }
+    //     // TODO: solve two winners problem
+    //     if (
+    //         electionsMapping[_electionID].winner.voteAmount <
+    //         electionsMapping[_electionID]
+    //             .addressToParticipant[_participant]
+    //             .voteAmount
+    //     ) {
+    //         electionsMapping[_electionID].winner.voteAmount = electionsMapping[
+    //             _electionID
+    //         ].addressToParticipant[_participant].voteAmount;
+    //         electionsMapping[_electionID].winner.winnerAddress = _participant;
+    //     }
+    //     emit Received(msg.sender, msg.value);
+    // }
 
-    function participate() public isElectionInitedMod isDeadlineReached {
-        require(
-            false == addressToParticipant[msg.sender].isParticipant,
-            "address is a participant already"
-        );
-        addressToParticipant[msg.sender].isParticipant = true;
-    }
+    // function withdrawPrize(uint256 _electionID)
+    //     public
+    //     payable
+    //     isParticipatingInElections
+    //     isPrizeWithdrawn(_electionID)
+    // {
+    //     uint256 fee = calculateFee();
+    //     uint256 prize = electionsMapping[_electionID].treasury - fee;
+    //     address payable _to = payable(msg.sender);
 
-    function vote(address _participant)
-        public
-        payable
-        isElectionInitedMod
-        isParticipant(_participant)
-        isDeadlineReached
-    {
-        require(
-            true != addressToParticipant[msg.sender].isVoted,
-            "address has voted already"
-        );
-        require(1000000000000000000 == msg.value, "amount is not right");
+    //     (bool sent, bytes memory data) = _to.call{value: prize}("");
+    //     require(sent, "Failed to send Ether");
 
-        emit Received(msg.sender, msg.value);
+    //     electionsMapping[_electionID].treasury -= prize;
+    //     electionsMapping[_electionID].isPrizeWithdrawn = true;
 
-        addressToParticipant[_participant].voteAmount += 1;
-        addressToParticipant[msg.sender].isVoted = true;
-        treasury += msg.value;
+    //     feeTreasury = electionsMapping[_electionID].treasury;
+    //     electionsMapping[_electionID].treasury = 0;
+    // }
 
-        // TODO: solve two winners problem
-        if (winner.voteAmount < addressToParticipant[_participant].voteAmount) {
-            winner.voteAmount = addressToParticipant[_participant].voteAmount;
-            winner.winnerAddress = _participant;
-        }
-    }
+    // function calculateFee() internal view returns (uint256) {
+    //     return (feePercentage * treasury) / 100;
+    // }
 
-    function withdrawPrize() public payable prizeWithdrawnMod {
-        uint256 fee = calcFee();
-        uint256 prize = treasury - fee;
+    // function withdrawFee() public payable onlyOwner {
+    //     require(isPrizeWithdrawn == false, "prize is not withdrawed");
 
-        address payable _to = payable(msg.sender);
-        (bool sent, bytes memory data) = _to.call{value: prize}("");
-        require(sent, "Failed to send Ether");
+    //     address payable _to = payable(msg.sender);
+    //     (bool sent, bytes memory data) = _to.call{value: feeTreasury}("");
+    //     require(sent, "Failed to send Ether");
+    //     feeTreasury = 0;
+    // }
 
-        treasury -= prize;
-        isPrizeWithdrawn = true;
-    }
+    constructor() {}
 
-    function calcFee() internal view returns (uint256) {
-        return (feePercentage * treasury) / 100;
-    }
-
-    function withdrawFee() public payable onlyOwner {
-        require(isPrizeWithdrawn == false, "prize is not withdrawed");
-
-        address payable _to = payable(msg.sender);
-        (bool sent, bytes memory data) = _to.call{value: treasury}("");
-        require(sent, "Failed to send Ether");
-        treasury = 0;
+    function deposit() public payable {
+        _asyncTransfer(this, msg.value);
     }
 }
